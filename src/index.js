7db6e73fe00d6664d43c672755738d94c877542a
[madhan2023] <[madhankumarjoth@gmail.com]>
2024-02-08 20:40:33 +0530

    try landing page in react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Body = () => {
+  return (
+    <div id="body">
+      <Header />
+      <Card
+      className='section'
+      img='.'
+      />
+      
+    </div>
+  );
+};
+
+const Header =()=>{
+  return(
+    <div className='header'>
+      <span className='header-title' >
+        This is top heading
+      </span>
+      <br/>
+      <span className="header-text" >
+        the second heading
+      </span>
+    </div>
+  )
+}
+
+const Card =(props) =>{
+  return(
+    <div className={props.className} >
+      <div className="small-div">
+        <i className={props.className}></i>
+        <img src={props.img} alt=''/>
+      </div>
+      <div className="big-div">
+        <span className='div-title'>
+          {props.title}
+        </span>
+        <br/>
+        <span>
+          {props.description}
+        </span>
+      </div>
+    </div>
+  )
+}
